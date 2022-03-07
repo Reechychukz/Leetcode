c1fdd4523892fd4f9f526280bad35e8a4012a89a
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2022-03-07 20:06:24 +0100

    Update FirstBadVersion.py

--- FirstBadVersion.py
+++ FirstBadVersion.py
@@ -3,16 +3,16 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        beg = 1
+        first = 0
         last = n
-        pos = 1
-        while beg <= last:
-            mid = (beg + last) // 2
+        pos = 0
+        while first <= last:
+            mid = (first + last) // 2
             x = isBadVersion(mid)
             if x == True:
                 pos = mid
                 last = mid - 1
             else:
-                beg = mid + 1
+                first = mid + 1
                 
         return pos
