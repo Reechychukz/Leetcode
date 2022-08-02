f58de74971db7020dcf3874c2203cfc8d9d7761e
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2022-08-03 00:09:52 +0100

    Update reverse string.js

--- javascript solutions/reverse string.js
+++ javascript solutions/reverse string.js
@@ -13,3 +13,20 @@ var reverseString = function(s) {
     }
     s.splice(0, originalLength);
 };
+
+/**
+ * Alternative solution
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let start = 0;
+    let end = s.length - 1;
+    while (start <= end) {
+        let temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+};
