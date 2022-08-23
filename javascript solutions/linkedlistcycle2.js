854541f0db585fceb884dc2e39d8d991a0bafcd9
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2022-08-23 01:01:11 +0100

    Create linkedlistcycle2.js
    Linked List Cycle II Leetcode

---
+++ javascript solutions/linkedlistcycle2.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let slow = head;
+    let fast = head;
+    
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow == fast) 
+            break;
+    }
+    
+    if(!fast || !fast.next) 
+        return null;
+    
+    fast = head;
+    while(fast){
+        if(slow == fast) 
+            return slow;
+        slow = slow.next;
+        fast = fast.next;
+    }
+};
