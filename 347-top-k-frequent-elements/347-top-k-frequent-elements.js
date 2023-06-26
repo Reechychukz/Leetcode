31bae0bacea6b6607f9bd9dd8a4ea4ba73881c07
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2023-06-26 16:33:28 +0100

    Time: 74 ms (70.18%), Space: 44.6 MB (87.48%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -4,18 +4,16 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    let frequency = {}
-    for( let i = 0; i < nums.length; i++){
-        if(frequency.hasOwnProperty(nums[i])) frequency[nums[i]] += 1;
-        else frequency[nums[i]] = 1;
+    let numsMap = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (!numsMap.has(nums[i])) numsMap.set(nums[i], 0);
+        numsMap.set(nums[i], numsMap.get(nums[i]) + 1);
     }
-    let result = Object.keys(frequency).map((key) => [Number(key), frequency[key]]);
-    let sortedResult = result.sort((a,b) => {
-        return b[1]-a[1]
-    })
-    let output = []
-    for ( let i = 0; i < k; i++){
-        output.push(sortedResult[i][0])
-    }
-    return output;
+   
+    // Convert the Map to an array of key-value pairs
+    const sortedMap = new Map([...numsMap].sort((a, b) => b[1] - a[1]));    
+    const sortedMapKeys = Array.from(sortedMap.keys());
+    return sortedMapKeys.slice(0, k);
+
 };
