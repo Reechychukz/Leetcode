275bbd0ed271e6e41bb13cd1202ee2125f0b3de5
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2023-07-05 23:06:57 +0100

    Time: 594 ms (5.48%), Space: 63.2 MB (36.39%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const numsSet = new Set(nums);
+    let finalSet = new Set();
+    let subLength = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        
+        if (!numsSet.has(nums[i] - 1)) {
+            let length = 0;
+            
+            while(numsSet.has(nums[i] + length)) {
+            length += 1
+            subLength = Math.max(length, subLength)
+            }
+        } 
+    }
+    return subLength;
+};
