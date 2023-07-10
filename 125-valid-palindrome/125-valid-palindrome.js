6b42614b8ad340eed8debe0f69b50043593314f2
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2023-07-10 09:31:08 +0100

    Time: 102 ms (8.18%), Space: 48.2 MB (27.42%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -2,15 +2,29 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    const newS = s.replace(/[\W_]/g, "").toLowerCase().split('');    
+var isPalindrome = function(s) {  
     let left = 0,
-        right = newS.length - 1;
+        right = s.length - 1;
     
-    while(left <= right) {
-        if (newS[left] !== newS[right]) return false;        
+    while(left < right) {
+        while ((left < right) && (!isAlphaNum(s[left]))) {
+            left++;
+        }
+        
+        while ((right > left) && (!isAlphaNum(s[right]))) {
+            right--;
+        }
+        
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
+
         left++;
         right--;
     }    
     return true;
 };
+
+var isAlphaNum = (s) => {
+     return (65 <= s.charCodeAt(0) && s.charCodeAt(0) <= 90) ||
+            (97 <= s.charCodeAt(0) && s.charCodeAt(0) <= 122) ||
+            (48 <= s.charCodeAt(0) && s.charCodeAt(0) <= 57);
+}
