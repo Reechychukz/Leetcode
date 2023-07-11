4209195942eaab008f2aec75b2d30bd99f940d6f
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2023-07-11 09:31:33 +0100

    Time: 947 ms (11.90%), Space: 65.2 MB (14.79%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -5,26 +5,25 @@
 var threeSum = function(nums) {
     
     let result = [];
-    let sortedNums = nums.sort((a,b) => a-b)
+    nums.sort((a,b) => a-b)
+    
     for (let i = 0; i < nums.length - 1; i++) {
-        if (i > 0 && nums[i] == nums[i-1]) continue;
-        let target = 0 - sortedNums[i],
+        console.log(nums)
+        if (i > 0 && nums[i] === nums[i-1]) continue;
+        let target = 0 - nums[i],
             l = i + 1,
             r = nums.length - 1;
         
-        while (l < r) {
+        while (l < r) {            
+            let sum = nums[l] + nums[r];
             
-            let sum = sortedNums[l] + sortedNums[r];
             if (sum === target) {
-                result.push([sortedNums[i], sortedNums[l], sortedNums[r]])
+                result.push([nums[i], nums[l], nums[r]])
                 l++
-                while(nums[l] === nums[l-1] && l < r) {
-                    l++
-                }
+                while(nums[l] === nums[l-1] && l < r) l++                
             }
             
-            else if (sum > target) r--
-            
+            else if (sum > target) r--            
             else l++
         }
     }
